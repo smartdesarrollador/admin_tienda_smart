f1f9ceffe5a17d9fbcc9828ac583b98b0f2ea454
Jeans <sistemadesignstyle@gmail.com>
2025-06-27 00:10:58 -0500

    cambiando url en environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: true,
-  urlDominioApi: 'https://apitiendamaqui.iatecdigital.com',
+  /* urlDominioApi: 'https://apitiendamaqui.iatecdigital.com',
   urlRaiz: 'https://apitiendamaqui.iatecdigital.com/api',
   apiUrl: 'https://apitiendamaqui.iatecdigital.com/api',
-  urlAssets: 'https://apitiendamaqui.iatecdigital.com/assets',
+  urlAssets: 'https://apitiendamaqui.iatecdigital.com/assets', */
+
+  urlDominioApi: 'https://apitienda.smartdigitaltec.com',
+  urlRaiz: 'https://apitienda.smartdigitaltec.com/api',
+  apiUrl: 'https://apitienda.smartdigitaltec.com/api',
+  urlAssets: 'https://apitienda.smartdigitaltec.com/assets',
 };
